beeba8f5eb4730845d4cc04cc8e3fd84ee71fa50
James <jamesgathuru@gmail.comgit config --global user.email jamesgathuru@gmail.com>
2019-03-25 15:26:21 +0300

    change the coding in the javascript and index files inorder to inacorporate styling

--- track.js
+++ track.js
@@ -1,32 +1,41 @@
-var length1 = [parseFloat(prompt('Input length1'))]
-  var length2 = [parseFloat(prompt('Input length2'))]
-  var length3 = [parseFloat(prompt('Input length3'))]
+function triangle() {
+  var length1 = document.getElementById('length1').value;
+  var length2 = document.getElementById('length2').value;
+  var length3 = document.getElementById('length3').value;
 
- var ab = length1.concat(length2);
+  var side1 = [parseFloat(length1)];
+  var side2 = [parseFloat(length2)];
+  var side3 = [parseFloat(length3)];
 
- var abc = ab.concat(length3) ;
+  var output = document.getElementById('output');
 
-var sorted = abc.sort(function (d, e) {
-return d - e;
-})
+  var ab = side1.concat(side2);
 
-var a = sorted[0]
-var b = sorted[1]
-var c = sorted[2]
+  var abc = ab.concat(side3);
 
-var lengthab = a + b;
+  var sorted = abc.sort(function(d, e) {
+    return d - e;
+  });
+
+  var a = sorted[0];
+  var b = sorted[1];
+  var c = sorted[2];
+
+  var lengthab = a + b;
 
 
-if(lengthab < c){
-  	alert("ERROR! Not a triangle")
+  if (lengthab < c) {
+    output.textContent = "ERROR! NOT A TRIANGLE";
+  } else if (a === b && b === c && a > 0 && b > 0 && c > 0) {
+    output.textContent = 'EQUILATERAL TRIANGLE';
+  } else if (a === b && b !== c || a !== b && b === c && a > 0 && b > 0 && c > 0) {
+    output.textContent = ('ISOCELES TRIANGLE');
+  } else if (a !== b && b !== c && (Number.isInteger(a) === true || Number.isInteger(b) === true || Number.isInteger(c) === true) && a > 0 && b > 0 && c > 0) {
+    output.textContent = 'SCALENE TRIANGLE';
+  } else {
+    output.textContent = "INVALID MEASUREMENTS!!";
+  };
+}
+function reset() {
+    document.getElementById('form').reset();
   }
-else if(a === b && b === c && a > 0 && b > 0 && c > 0){
-  console.log('Equilateral Triangle')
-  alert('Equilateral Triangle')
-}else if(a === b && b !== c || a !== b && b === c && a > 0 && b > 0 && c > 0){
-  console.log('Isosceles Triangle')
-  alert('Isoceles Triangle')
-}else if(a !== b && b!== c && (Number.isInteger(a) === true || Number.isInteger(b) === true || Number.isInteger(c) === true) && a > 0 && b > 0 && c > 0){
-  console.log('Scalene Triangle')
-  alert('Scalene Triangle')
-}else { alert ("Invalid measurements. Kindly re-evaluate your figures.")};
